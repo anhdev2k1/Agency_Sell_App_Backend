09cd0863c59897d33c9334ef77a18034f12a4fd7
anhdev2k1 <info.anhdev@gmail.com>
2023-02-22 22:36:02 +0700

    create and setup mongoose DB

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
+import DBMongoose from "./configs/index.js";
 const app = express();
 const port = 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World! 123');
+app.get("/", (req, res) => {
+  res.send("Hello World! 123");
 });
-
+DBMongoose();
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`The app is listening on port ${port}`);
 });
